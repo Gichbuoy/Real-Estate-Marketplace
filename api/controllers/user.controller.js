93399072e437a62f4ffaea666d363c71d4aa1466
Alex Maina <lexizgichbuoy@gmail.com>
2023-11-29 11:19:05 +0300

    signup-api-route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,4 +2,4 @@ export const test = (req, res) => {
     res.json({
         message: 'Hello Pirate',
     });
-}
+};
--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
+// This file sets up a server using the Express framework, 
+// connects to a MongoDB database, and defines routes for user and authentication operations.
 
+
+// Connect to MongoDB
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
@@ -16,9 +22,16 @@ mongoose
 
 const app = express();
 
+app.use(express.json()); // allwos json as input of server
+
+
+// Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     }
 );
 
+
+// Define routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
