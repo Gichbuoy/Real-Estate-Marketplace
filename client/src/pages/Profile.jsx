638e00e479c0e8d30cabb6cec92cfdaf0a234390
Alex Maina <lexizgichbuoy@gmail.com>
2023-12-06 14:49:03 +0300

    upload img functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,62 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user)
+  const fileRef = useRef(null);
+  const {currentUser} = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  console.log(filePerc);
+  console.log(file);
+  
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+  useEffect(() => {
+    if(file) {
+      handleFIleUpload(file);
+    }
+  }, [file]);
+
+  const handleFIleUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / 
+        snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      }
+    );
+  }
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
         Profile
       </h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile"
-        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
+        <input
+        onChange={(e) => setFile(e.target.files[0])} 
+          type="file" 
+          ref={fileRef} 
+          hidden 
+          accept='image/*' 
+        /> 
+        <img 
+          onClick={()=>fileRef.current.click()} 
+          src={currentUser.avatar} 
+          alt="profile"
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
+        />
         <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
         <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
         <input type="text" placeholder='password' id='password' className='border p-3 rounded-lg' />
