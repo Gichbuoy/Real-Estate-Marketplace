f3fbf61fc1cc1b50ec2df3397054cca3e5af9639
Alex Maina <lexizgichbuoy@gmail.com>
2023-12-08 16:44:15 +0300

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ const app = express();
 
 app.use(express.json()); // allwos json as input of server
 
+app.use(cookieParser());
 
 // Start the server
 app.listen(3000, () => {
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,15 @@
+import { errorHandler } from "./error.js";
+import jwt from 'jsonwebtoken';
+
+export const  verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    
+    if (!token) return next(errrorHandler(401, "Invalid token"));
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return next(errorHandler(403, 'Forbidden'));
+
+        req.user = user;
+        next();
+    });
+};
