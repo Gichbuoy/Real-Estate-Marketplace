6f5d188eb083fbb1496e7b3392df7af37eb5ba91
Alex Maina <lexizgichbuoy@gmail.com>
2023-12-18 16:39:45 +0300

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -37,6 +38,8 @@ app.listen(3000, () => {
 // Define routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 
 /*
