f410e57a41ae0dcb7dfb16ff72e6871015c48012
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-05 00:45:13 +0300

    listing api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
     res.json({
@@ -40,5 +41,18 @@ export const deleteUser = async (req, res, next) => {
         res.status(200).json('User has been deleted!');
     } catch (error) {
         next(error);
+    }
+};
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error)
+        }
+    } else {
+        return next(errorHandler(401, 'You can only view your own listing!'));
     }
 };
