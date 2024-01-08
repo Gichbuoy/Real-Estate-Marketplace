fe73dae96a4deaf6182712fd43584852e1350f83
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-09 00:24:26 +0300

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -265,9 +265,11 @@ export default function Profile() {
                   <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>
                     Delete
                   </button>
-                  <button className='text-green-700 uppercase'>
-                    Edit
-                  </button>
+                  <Link to={`/update-listing/${listing._id}`}>
+                    <button className='text-green-700 uppercase'>
+                      Edit
+                    </button>
+                  </Link>
                 </div>
             </div>
           ))}
