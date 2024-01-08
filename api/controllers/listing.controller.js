92d2173ea21dba589d8435bb93ce7fa101cebd91
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-08 21:53:55 +0300

    update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -28,3 +28,26 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listing!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}
