2d2a8f8684802c67f4cba484c0faff46b4b8e7ea
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-10 14:32:25 +0300

    create seach api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -63,3 +63,53 @@ export const getListing = async (req, res, next) => {
         next(error)
     }
 };
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        
+        let offer = req.query.offer
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent', 'lease'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' }, // i means regex search functionality wont care about lower or uppercase
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+
+        // const listings = await Listing.find();
+        // res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+};
