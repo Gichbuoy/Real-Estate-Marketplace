e719637b3f11392a42e9ca7e655f63679014837f
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-11 03:02:04 +0300

    listing item components

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -95,7 +95,7 @@ export default function Listing(){
                         </p>
                         {listing.offer && (
                             <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                            Ksh.{+listing.regularPrice - +listing.discountPrice} OFF
+                            Ksh. {+listing.regularPrice - +listing.discountPrice} OFF
                             </p>
                         )}
                     </div>
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
     const navigate = useNavigate();
@@ -193,10 +194,26 @@ export default function Search() {
             </form>
         </div>
 
-        <div className=''>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
                 Listing results:
             </h1>
+            <div className='p-6 flex flex-wrap gap-4'>
+                {!loading && listings.length === 0 && (
+                    <p className='text-xl text-red-700'>No listing found!</p>
+                )}
+                
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                )}
+
+                {!loading && 
+                    listings && 
+                    listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing} />
+                    ))
+                }
+            </div>
         </div>
     </div>
   )
