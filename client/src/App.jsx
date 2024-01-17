54aeb69164241d51895a52eb90aed27396e04c97
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-17 15:42:48 +0300

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -28,6 +29,8 @@ export default function App() {
           <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
+    
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,65 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export default function Footer() {
+  const {currentUser} = useSelector(state => state.user);
+
+
+
+  return (
+    <footer className='bg-slate-300 shadow-md mt-auto'>
+      <div className='flex flex-wrap justify-between items-center max-w-6xl mx-auto p-3'>
+        
+        <div className='flex sm:flex-wrap gap-4 sm:gap-2'>
+            <Link to='/'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>FindMy</span>
+                <span className='text-green-700'>Home</span>
+            </h1>
+            </Link>
+            <Link to='/profile'>
+                {currentUser ? (
+                <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile'/>
+            ): (
+                <li className='text-slate-700 hover:underline'>Sign In</li>
+                )}
+            </Link>
+            
+            
+
+        </div>
+
+
+        <div className='flex flex-col'>
+          <h4></h4>
+          <a href="/" className='text-slate-700 hover:underline'>Terms of Service</a>
+          <a href="/" className='text-slate-700 hover:underline'>Privacy Policy</a>
+          <a href="/" className='text-slate-700 hover:underline'>License</a>
+          <a href="/" className='text-slate-700 hover:underline'>Blogs</a>
+        </div>
+        
+        <div className=''>
+            <ul className='flex flex-col gap-2'>
+            <Link to='/' onClick={() => window.scrollTo(0, 0)}>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>
+                Home
+                </li>
+            </Link>
+            <Link to='/about' onClick={() => window.scrollTo(0, 0)}>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>
+                About
+                </li>
+            </Link>
+
+            <Link to='/'>
+                <li className='hidden sm:inline text-slate-700 hover:underline'>
+                Contact Us
+                </li>
+            </Link>
+            
+            </ul>
+        </div>
+      </div> 
+    </footer>
+  )
+}
