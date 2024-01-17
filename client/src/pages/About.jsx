5cc4efb9a140663e84654a862c25a6a8d9a0d05d
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-17 14:06:15 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,71 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Find My Home
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Welcome to Find My Home, your trusted partner in finding the perfect home that suits your lifestyle and preferences. At Find My Home, we understand that a home is not just a place; it's a reflection of your dreams, aspirations, and the foundation for countless memories.
+        <br/>
+        <br />
+        Find My Home is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Our Mission
+      </h1>
+      <p className='mb-4 text-slate-700'>
+      Our mission is simple: to simplify the home-searching process and connect you with the property of your dreams. We help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, lease, or rent a property, we are here to help you every step of the way.
+      We strive to provide a seamless and enjoyable experience, making your journey towards finding the ideal home an exciting adventure.
+      </p>
+
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Who We Are
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Find My Home is a team of passionate real estate professionals dedicated to helping you navigate the complexities of the real estate market. With years of industry experience, we bring expertise, innovation, and a client-centric approach to every interaction. 
+      </p>
+
+      <h1 className='text-3xl font-bold mb-2 mt-6 text-slate-800'>
+        What Sets Us Apart:
+      </h1>
+      <h2 className='text-2xl font-semibold mb-2 text-slate-800'>
+        * Comprehensive Listings
+      </h2>
+      <p className='mb-4 text-slate-700'>
+        Our platform boasts an extensive database of properties, ensuring you have access to a wide range of homes that match your criteria. Whether you're looking for a cozy apartment, a spacious family home, or an investment property, we've got you covered.      
+      </p>
+
+      <h2 className='text-2xl font-semibold mb-2 text-slate-800'>
+        * User-Friendly Interface
+      </h2>
+      <p className='mb-4 text-slate-700'>
+        We believe in the power of simplicity. Our user-friendly website and intuitive search features make it easy for you to explore properties, save favorites, and track your home-buying journey effortlessly. 
+      </p>
+
+      <h2 className='text-2xl font-semibold mb-2 text-slate-800'>
+        * Expert Guidance
+      </h2>
+      <p className='mb-4 text-slate-700'>
+        Navigating the real estate market can be overwhelming. That's why our team of expert agents is here to guide you at every step. From understanding market trends to negotiating the best deals, we are committed to providing you with the support you need.      
+      </p>
+
+      <h1 className='text-3xl font-bold mt-6 mb-4 text-slate-800'>
+        Our Commitment      
+      </h1>
+      <p className='mb-4 text-slate-700'>
+      Find My Home is built on the foundation of trust, transparency, and commitment to our clients. We believe in forging lasting relationships and take pride in being a reliable resource throughout your home-buying journey.        
+      <br />
+      <br />
+        Join us at Find My Home, where your dream home is just a click away. Explore our listings, connect with our experienced agents, and embark on the exciting adventure of finding your perfect home.
+        <br />
+        <br />
+        Thank you for choosing Find My Home. Your journey to finding the home of your dreams begins here!
+      </p>
+      
+      
+
+    </div>
   )
 }
