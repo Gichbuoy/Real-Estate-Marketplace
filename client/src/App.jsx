dbce21782f6f5b1f80758ddda305058f49bd11d6
Alex Maina <lexizgichbuoy@gmail.com>
2024-01-20 01:47:01 +0300

    resolve issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
